89d7b583415a0d188916ff86f80ab36ae2670438
T.Oka <chita123daa@gmail.com>
2020-09-01 13:25:38 +0900

    リポジトリ名変更、（inquiry_formだと見積申請書に近い）

--- index.php
+++ index.php
@@ -1,11 +1,71 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=1024, initial-scale=1.0">
   <title>Trussardi</title>
 </head>
 <body>
-  
+  <main>
+    
+      <header>
+        <h1>Trussardi</h1>
+        <nav>
+          <ul>
+            <li>営業時間：11:00 〜 21:00</li>
+            <li>TEL：○○○-○○○-○○○○</li>
+          </ul>
+        </nav>
+      </header>
+      <section>
+        <h2>メニュー</h2>
+        <article>
+        <h3>パスタ</h3>
+        <img src="./img/pasta.jpg" alt="パスタpic">
+        <nav>
+          <ul>
+            <li>カルボナーラ</li>
+            <li>800円</li>
+            <li>ペペロンチーノ</li>
+            <li>750円</li>
+            <li>和風明太子パスタ</li>
+            <li>860円</li>
+            <li>イカスミパスタ</li>
+            <li>820円</li>
+            <li>ジェノベーゼ</li>
+            <li>850円</li>
+          </ul>
+        </nav>
+        </article>
+        <article>
+        <h3>ピザ</h3>
+        <img src="./img/pizza.jpg" alt="ピザpic">
+        <nav>
+          <ul>
+            <li>マルゲリータ</li>
+            <li>1200円</li>
+            <li>シーフードピザ</li>
+            <li>1300円</li>
+            <li>ジェノベーゼ</li>
+            <li>1300円</li>
+            <li>マスカルポーネピザ</li>
+            <li>1400円</li>
+          </ul>
+        </nav>
+        </article>
+      </section>
+      <section>
+        <h2>アクセス</h2>
+        <p>杜王町駅から西口から徒歩7分</p>
+        <img src="./img/map.jpg" alt="マップpic">
+        <h2>お問い合わせはこちらから</h2>
+        <button id="contact-button">お問い合わせフォーム</button>
+      </section>
+      <footer>
+        <p>著者の情報</p>
+      </footer>
+  </main>
 </body>
+
 </html>
