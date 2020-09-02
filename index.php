06eb498cc1a0256526bff094c11899265a630f31
T.Oka <chita123daa@gmail.com>
2020-09-02 23:48:25 +0900

    トップページのデザインを指定　flexboxでデザイン、レスポンシブは未指定

--- index.php
+++ index.php
@@ -3,67 +3,91 @@
 
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=1024, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./index.css" />
   <title>Trussardi</title>
 </head>
 <body>
   <main>
-    
       <header>
-        <h1>Trussardi</h1>
-        <nav>
-          <ul>
-            <li>営業時間：11:00 〜 21:00</li>
-            <li>TEL：○○○-○○○-○○○○</li>
-          </ul>
-        </nav>
+        <div class="header__float">
+          <h1>Trussardi</h1>
+          <nav>
+            <ul>
+              <li id="open-time" style="font-size:1em">営業時間：11:00 〜 21:00</li>
+              <li id="tel-num" style="font-size:1em">TEL：○○○-○○○-○○○○</li>
+            </ul>
+          </nav>
+        </div>
       </header>
+      <section class="top-pic"></section>
       <section>
-        <h2>メニュー</h2>
-        <article>
+        <article  class="menu-bar">
+          <h2>メニュー</h2>
+        </article>
+        <article class="pasta-menu">
         <h3>パスタ</h3>
-        <img src="./img/pasta.jpg" alt="パスタpic">
+        <img src="../contact_form/img/pasta.jpg" alt="パスタpic">
         <nav>
-          <ul>
-            <li>カルボナーラ</li>
-            <li>800円</li>
-            <li>ペペロンチーノ</li>
-            <li>750円</li>
-            <li>和風明太子パスタ</li>
-            <li>860円</li>
-            <li>イカスミパスタ</li>
-            <li>820円</li>
-            <li>ジェノベーゼ</li>
-            <li>850円</li>
+          <ul class="menu-list">
+            <div>
+              <li>カルボナーラ</li>
+              <li>800円</li>
+            </div>
+            <div>
+              <li>ペペロンチーノ</li>
+              <li>750円</li>
+            </div>
+            <div>
+              <li>和風明太子パスタ</li>
+              <li>860円</li>
+            </div>
+            <div>
+              <li>イカスミパスタ</li>
+              <li>820円</li>
+            </div>
+            <div>
+              <li>ジェノベーゼ</li>
+              <li>850円</li>
+            </div>
           </ul>
         </nav>
         </article>
-        <article>
+        <article  class="pizza-menu">
         <h3>ピザ</h3>
-        <img src="./img/pizza.jpg" alt="ピザpic">
+        <img src="../contact_form/img/pizza.jpg" alt="ピザpic">
         <nav>
-          <ul>
-            <li>マルゲリータ</li>
-            <li>1200円</li>
-            <li>シーフードピザ</li>
-            <li>1300円</li>
-            <li>ジェノベーゼ</li>
-            <li>1300円</li>
-            <li>マスカルポーネピザ</li>
-            <li>1400円</li>
+          <ul class="menu-list">
+            <div>
+              <li>マルゲリータ</li>
+              <li>1200円</li>
+            </div>
+            <div>
+              <li>シーフードピザ</li>
+              <li>1300円</li>
+            </div>
+            <div>
+              <li>ジェノベーゼ</li>
+              <li>1300円</li>
+            </div>
+            <div>
+              <li>マスカルポーネピザ</li>
+              <li>1400円</li>
+            </div>
           </ul>
         </nav>
         </article>
       </section>
-      <section>
+      <section class="access-route">
         <h2>アクセス</h2>
         <p>杜王町駅から西口から徒歩7分</p>
-        <img src="./img/map.jpg" alt="マップpic">
+        <img src="../contact_form/img/map.jpg" alt="マップpic">
         <h2>お問い合わせはこちらから</h2>
         <button id="contact-button">お問い合わせフォーム</button>
       </section>
       <footer>
-        <p>著者の情報</p>
+        <p>Trussardi</p>
       </footer>
   </main>
 </body>
